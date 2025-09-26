ec03fd5f79a7686cd7cd801f406839ca3ab73eca
ssanjose@hiberus.com <ssanjose@hiberus.com>
2025-09-26 11:23:17 +0200

    Fosas - Ajustes maquetación intro despues migracion next

--- components/intro/introScreen1.jsx
+++ components/intro/introScreen1.jsx
@@ -4,30 +4,26 @@ import barraIntro from "../../app/assets/barra-intro.svg";
 import ScrollButton from "../ScrollButton/ScrollButton";
 import "../../app/styles/intro/_introScreen1.scss";
 
-export default function IntroScreen1({ onNextScreen, onNavigation }) {
-  return (
-    <section className="intro-screen" id="screen1" aria-label="Introducción">
-      <div className="intro-sticky">
-        <div className="intro-overlay">
-          <h1>El país de las 6.000 fosas</h1>
-          <img
-            className="intro-divider-img"
-            src={barraIntro?.src || barraIntro}
-            alt=""
-            aria-hidden="true"
-          />
-          <p className="intro-sub">AQUÍ YACE LA HISTORIA</p>
-        </div>
-        <ScrollButton
-          label="Conoce las historias"
-          animated={true}
-          icon="mouse"
-          onClick={() => {
-            if (onNavigation) onNavigation({ action: "next-screen" });
-            else if (onNextScreen) onNextScreen({ action: "next-screen" });
-          }}
-        />
-      </div>
-    </section>
-  );
+export default function IntroScreen1({onNextScreen, onNavigation}) {
+    return (
+        <section className="intro-screen" id="screen1" aria-label="Introducción">
+            <div className="intro-sticky">
+                <div className="intro-overlay">
+                    <h1>El país de las 6.000 fosas</h1>
+                    <div className="divider-intro" aria-hidden="true"></div>
+
+                    <p className="intro-sub">AQUÍ YACE LA HISTORIA</p>
+                </div>
+                <ScrollButton
+                    label="Conoce las historias"
+                    animated={true}
+                    icon="mouse"
+                    onClick={() => {
+                        if (onNavigation) onNavigation({action: "next-screen"});
+                        else if (onNextScreen) onNextScreen({action: "next-screen"});
+                    }}
+                />
+            </div>
+        </section>
+    );
 }
